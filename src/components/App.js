dcc7bc75050785636b23102f5facdbe5bceef437
shinshin86 <beagles1986@gmail.com>
2018-03-23 06:39:13 +0900

    Add header

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './Header'
+import GifSearch from '../containers/GifSearch'
+
+const App = () => (
+  <div>
+    <Header />
+    <GifSearch />
+  </div>
+)
+
+export default App
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import "@babel/polyfill"
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import App from './containers/App'
+import App from './components/App'
 import configureStore from './store'
 import rootSaga from './sagas'
 
